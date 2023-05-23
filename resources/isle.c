68ef96230b468ad735d1d85e1ce73504f5ccf659
izenynn <me@izenynn.com>
2023-05-23 13:26:23 +0200

    feat: add util programs

---
+++ resources/isle.c
@@ -0,0 +1,18 @@
+/*
+ * Check if a system is little-endian
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 1;
+
+	// Little-endian if true
+	if(*(char *)&n == 1)
+		printf("Little-endian\n");
+	else
+		printf("Big-endian\n");
+
+	return 0;
+}
